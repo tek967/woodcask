aad475fcb871b1457cdde1439ce1b29cd9501be4
EasonTek <easontekreviews@icloud.com>
2021-04-23 08:55:44 +0800

    1.1

--- WebDisplayEngine/ViewController.swift
+++ WebDisplayEngine/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  WebDisplayEngine
+//  wde
 //
 //  Created by EasonTek on 23/1/21.
 //
@@ -10,31 +10,63 @@ import WebKit
 class ViewController: UIViewController {
     var loadingStatus:Float = 0.0
     @IBOutlet weak var webView: WKWebView!
-    @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var progressSpinner: UIActivityIndicatorView!
     @IBOutlet weak var progressLabel: UILabel!
-    var fieldurl = "https://www.xflymusic.com/"
+    var toggleState:Bool = true
+    
+    @IBAction func toggler(_ sender: UISwitch) {
+        if toggleState == false{
+            toggleState = true
+            print("yes")
+        } else if toggleState == true{
+            toggleState = false
+            print("no")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //uncomment to load remote page
-       
-        if let url = URL(string: fieldurl) {
-           let request = URLRequest(url: url)
-           webView.load(request)
-           webView.allowsBackForwardNavigationGestures = true
-           webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        if let url = URL(string: "https://www.google.com/") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+            webView.allowsBackForwardNavigationGestures = true
+            webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
         }
+        
+        
     }
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "estimatedProgress" {
-           loadingStatus = Float(webView.estimatedProgress)
-            if loadingStatus < 0.999 {
-                progressBar.isHidden = false
-                progressBar.progress = loadingStatus
-            } else if loadingStatus > 0.999 {
-              progressBar.progress = 0
-              progressBar.isHidden = true
+            loadingStatus = Float(webView.estimatedProgress)
+            if loadingStatus < 0.8 {
+                progressLabel.isHidden = false
+                progressSpinner.isHidden = false
+                progressSpinner.startAnimating()
+                progressLabel.text = String(loadingStatus * 100) + "%"
+                if toggleState == true {
+                    webView.isHidden = true
+                } else if toggleState == false {
+                    webView.isHidden = false
+                }
+            } else if loadingStatus > 0.8 {
+                if toggleState == true {
+                    webView.isHidden = false
+                    progressSpinner.stopAnimating()
+                    progressLabel.text = "%"
+                    progressSpinner.isHidden = true
+                    progressLabel.isHidden = true
+                } else if toggleState == false {
+                    
+                    print("NONONONO")
+                    progressSpinner.stopAnimating()
+                    progressLabel.text = "%"
+                    progressSpinner.isHidden = true
+                    progressLabel.isHidden = true
+                }
             }
+            
         }
     }
+
 }
+
